74e6dd7691c5418c311b0fc6fe5d6a274ee649c6
obrok <pawel.obrok@gmail.com>
2012-08-04 22:40:36 +0200

    Perplexity calculations for LDA and preliminary results

--- thesis/model/lsa_model.py
+++ thesis/model/lsa_model.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import os
+import math
+from operator import itemgetter
 from gensim import corpora, models, similarities
 
 STOPLIST = set(["NUMBER", ".", "", ",", "-", ")", "(", "\"", u"być", "a"])
@@ -21,6 +23,8 @@ class ModelWrapper:
     self.model = model(self.vectors, id2word=self.dictionary, num_topics=topics)
     self.vectors = self.model[self.vectors]
     self.index = self.index = similarities.MatrixSimilarity(self.vectors)
+    self.topics = {}
+    self.topic_no = topics
 
   def vectorize(self, doc):
     return self.dictionary.doc2bow(doc)
@@ -37,3 +41,37 @@ class ModelWrapper:
     sims = self.index[self.process(doc)]
     sims = sorted(enumerate(sims), key=lambda x: -x[1])
     return map(lambda x: x[0], sims)
+
+  def get_topic(self, topic_index):
+    if not topic_index in self.topics:
+      self.topics[topic_index] = self.model.show_topic(topic_index, len(self.dictionary))
+    return self.topics[topic_index]
+
+  def perplexity(self, docs):
+    perplexity = 0.0
+
+    for doc in docs:
+      vector = self.process(doc)
+      total = sum(map(lambda x: x[1], vector))
+      doc_logprob = 0.0
+
+      for word in doc:
+        word_prob = 0.0
+        word_vector = self.process([word])
+        for (topic_index, weight) in word_vector:
+          topic = self.get_topic(topic_index)
+          topic_total = sum(map(itemgetter(0), topic))
+          topic_entry = filter(lambda x: x[0] == topic_index, vector)
+          word_entry = filter(lambda x: x[1] == word, topic)
+          if len(word_entry) > 0 and len(topic_entry) > 0:
+            weight = word_entry[0][0]
+            topic_weight = topic_entry[0][1]
+            word_prob += topic_weight / total * weight / topic_total
+
+        if word_prob > 0:
+          doc_logprob += math.log(word_prob)
+
+      perplexity += doc_logprob / len(doc)
+
+    return math.exp(-perplexity / len(docs))
+
