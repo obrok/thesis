a3eb993f490cbee513ed44016fc0e3075ef72ae4
obrok <pawel.obrok@gmail.com>
2012-09-13 08:57:11 +0200

    Topic time trial results

--- bin/time.sh
+++ bin/time.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-bin/time_docs data/pap-tagged.txt LdaModel > results/lda_time_docs
 bin/time_topics data/pap-tagged.txt LdaModel > results/lda_time_topics
-bin/time_docs data/pap-tagged.txt LsiModel > results/lsi_time_docs
 bin/time_topics data/pap-tagged.txt LsiModel > results/lsi_time_topics
